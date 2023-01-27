7b2a309d8dc2d5af92af3cf24a0485c813949d13
Ash <acst52@gmail.com>
2023-01-27 13:49:02 -0500

    Fixed LC code, added rest of LC code
    LC = local storage
    Also removed a few commented out lines
    Code is now functional!!!

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
 $(function () {
  
 // First, let's get the current date & time to display in the header:
-
 let currentDay = document.getElementById("currentDay");
 
 // let's update the time every 30 seconds?
@@ -16,9 +14,6 @@ setInterval(() => {
 // let's add and remove the past/present/future classes based on time:
 function hourUpdater() {
 let currentHour = dayjs().hour();
-// for (let id in divTimes) {
-//   let hour = parseInt(id.split("-")[1]);
-//   let div = document.getElementById(id);
 $(".time-block").each(function(){ // loop though each of the time blocks
 const hour = parseInt($(this).attr("id").split("-")[1]); // split id to get hour to compare to curr hour:
    if (hour < currentHour) {
@@ -36,19 +31,31 @@ const hour = parseInt($(this).attr("id").split("-")[1]); // split id to get hour
 hourUpdater();
 setInterval(hourUpdater, 10000);
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+// Now lets add a click event listener to each save button that triggers a fcn which:
+   // 1. grabs the user's text input from each description,
+   // 2. associates their input with the divID of its parent, and finally
+   // 3. saves the div ID + user input to local storage:
+let saveButtons = document.getElementsByClassName("saveBtn");
+for (let i = 0; i < saveButtons.length; i++) {
+  saveButtons[i].addEventListener("click", function() {
+    let textArea = this.parentNode.getElementsByClassName("description")[0];
+    let userInput = textArea.value;
+      console.log(userInput);
+    let divID = this.parentNode.id;
+      console.log(divID);
+    localStorage.setItem(divID, userInput);
+  });
+}
 
-let saveButton = document.getElementsByClassName("saveBtn");
-
-saveButton.addEventListener("click", function(){
-  let textArea = document.getElementsByClassName("description")[0];
-  let userInput = textArea.value;
-  localStorage.setItem("userInput", userInput);
-});
-
-// ... to apply that code to accommodate all textareas / divs, you have to associate it 
-  // with the div ID so we can save & render it there on refresh!
-
-});
+// Finally, let's render user's saved input onto the divs using localStorage.getItem(key) method.
+  // key = div id; we want to set value of textarea element as stored userInput from above.
+let textAreas = document.getElementsByClassName("description");
+for (let i = 0; i < textAreas.length; i++) {
+  let divID = textAreas[i].parentNode.id;
+  let savedInput = localStorage.getItem(divID);
+  if (savedInput) {
+    textAreas[i].value = savedInput;
+  }
+}
+  
+}); // this closes the $function at start of script
